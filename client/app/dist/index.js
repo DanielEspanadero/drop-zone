b11df8c06eef9649202c909435133e4a8b57296f
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-07 00:31:00 +0200

    Drag and drop zone created

--- client/app/dist/index.js
+++ client/app/dist/index.js
@@ -4,3 +4,47 @@
 //TODO Realizar validaciones desde el backend
 //TODO Realizar validaciones desde el frontend
 //TODO Optimizar código
+const dropArea = document.querySelector('#drop-area');
+const dragText = document.querySelector('h2');
+const button = document.querySelector('button');
+const input = document.querySelector('#input-file');
+let files;
+button.addEventListener('click', () => {
+    input.click();
+});
+input.addEventListener('change', (e) => {
+    e.preventDefault();
+    files = input.files;
+    dropArea.classList.add('active');
+    showFile(files);
+    dropArea.classList.remove('active');
+});
+dropArea.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    dropArea.classList.add('active');
+    dragText.textContent = 'Drop to upload the files.';
+});
+dropArea.addEventListener('dragleave', (e) => {
+    e.preventDefault();
+    dropArea.classList.remove('active');
+    dragText.textContent = 'Drag and drop files.';
+});
+dropArea.addEventListener('drop', (e) => {
+    e.preventDefault();
+    dropArea.classList.remove('active');
+    dragText.textContent = 'Drag and drop files.';
+});
+let showFile = (files) => {
+    if (files.length === undefined) {
+        processFile(files);
+    }
+    else {
+        for (const file of files) {
+            processFile(file);
+        }
+        ;
+    }
+    ;
+};
+let processFile = (file) => {
+};
--- client/app/index.ts
+++ client/app/index.ts
@@ -3,3 +3,56 @@
 //TODO Realizar validaciones desde el backend
 //TODO Realizar validaciones desde el frontend
 //TODO Optimizar código
+
+
+const dropArea: any = document.querySelector('#drop-area');
+const dragText: any = document.querySelector('h2');
+const button: any = document.querySelector('button')!;
+const input: any = document.querySelector('#input-file')!;
+let files;
+
+button.addEventListener('click', () => {
+    input.click();
+});
+
+input.addEventListener('change', (e: any) => {
+    e.preventDefault();
+    files = input.files;
+    dropArea.classList.add('active');
+    showFile(files);
+    dropArea.classList.remove('active');
+});
+
+dropArea.addEventListener('dragover', (e: any) => {
+    e.preventDefault();
+    dropArea.classList.add('active');
+    dragText.textContent = 'Drop to upload the files.';
+
+});
+
+dropArea.addEventListener('dragleave', (e: any) => {
+    e.preventDefault();
+    dropArea.classList.remove('active');
+    dragText.textContent = 'Drag and drop files.';
+});
+
+dropArea.addEventListener('drop', (e: any) => {
+    e.preventDefault();
+    dropArea.classList.remove('active');
+    dragText.textContent = 'Drag and drop files.';
+});
+
+let showFile = (files: any) => {
+    if (files.length === undefined) {
+        processFile(files);
+    } else {
+        for (const file of files) {
+            processFile(file);
+        };
+    };
+};
+
+let processFile = (file: any) => {
+
+
+}
